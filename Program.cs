7f249d21a667db269891bbdbe175e0ba554b8fb7
wesleysotnas64 <wesleysotnas64@gmail.com>
2022-08-19 22:59:24 -0300

    Primeiro commit openTk.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OpenTK_4_Learning
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
